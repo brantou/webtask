eae43824ac2ba18f186a1a665ca0f26e00fd2c20
brantou <brantou89@gmail.com>
2017-10-22 15:26:23 +0800

    Updating reading.js

--- reading.js
+++ reading.js
@@ -9,4 +9,34 @@ app.get('/', function (req, res) {
   res.send('hello world');
 });
 
+app.get('/comment', function (req, res) {
+  console.log(req.query);
+  console.log(req.body);
+  res.sendStatus(200);
+});
+
+app.get('/highlight', function (req, res) {
+  console.log(req.query);
+  console.log(req.body);
+  res.sendStatus(200);
+});
+
+app.get('/archive', function (req, res) {
+  console.log(req.query);
+  console.log(req.body);
+  res.sendStatus(200);
+});
+
+app.get('/like', function (req, res) {
+  console.log(req.query);
+  console.log(req.body);
+  res.sendStatus(200);
+});
+
+app.get('/tag', function (req, res) {
+  console.log(req.query);
+  console.log(req.body);
+  res.sendStatus(200);
+});
+
 module.exports = Webtask.fromExpress(app);
